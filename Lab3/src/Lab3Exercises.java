ec056d01e423f3286f1e4dd02c189092a70161d7
lucas <lucas@DESKTOP-UPU1ICV>
2017-02-10 11:47:40 -0500

    Changed title and set window to open at center of the screen.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,11 @@
 import javax.swing.JFrame;
 
 public class Lab3Exercises {
-	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
-		myFrame.setVisible(true);
-	}
+    public static void main(String[] args) {
+        JFrame myFrame = new JFrame("Lucas");
+        myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        myFrame.setLocation(800, 500);
+        myFrame.setSize(400, 100);
+        myFrame.setVisible(true);
+    }
 }
